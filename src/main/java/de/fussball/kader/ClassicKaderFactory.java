4e9fcf3daa0e16c9992e2c34fe4f00a409bb1974
Florian <dekontaminator@googlemail.com>
2015-09-25 18:33:47 +0200

    cache

--- src/main/java/de/fussball/kader/ClassicKaderFactory.java
+++ src/main/java/de/fussball/kader/ClassicKaderFactory.java
@@ -3,7 +3,9 @@ package de.fussball.kader;
 import java.io.InputStream;
 import java.net.URL;
 import java.text.Normalizer;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
@@ -15,29 +17,36 @@ import org.jsoup.select.Elements;
 
 public class ClassicKaderFactory {
 
+	public static Map<String, String> cached = new HashMap<String, String>();
 
 	public Set<String> get(String id) {
 		try {
-
+			String html = null;
+			if (!cached.containsKey(id)) {
 				String urlString = "http://www.comunio.de/playerInfo.phtml?pid="
-						+ id ;
+						+ id;
 				InputStream is = (InputStream) new URL(urlString).getContent();
-				String html = IOUtils.toString(is, "UTF-8");
-				
+				html = IOUtils.toString(is, "UTF-8");
+
 				html = Normalizer.normalize(html, Normalizer.Form.NFD);
 				html = html.replaceAll("[^\\p{ASCII}]", "");
-				
+				cached.put(id, html);
+			}else{
+				html = cached.get(id);
+			}
+
 			Document doc = Jsoup.parse(html);
 			Elements lines = doc.select(".name_cont");
-			
+
 			Set<String> teamList = new HashSet<String>();
 			for (int i = 0; i < lines.size(); i++) {
 				Element line = lines.get(i);
 				String tempName = line.html();
 				tempName = StringEscapeUtils.unescapeHtml(tempName);
-				String norm = Normalizer.normalize(tempName, Normalizer.Form.NFD);
+				String norm = Normalizer.normalize(tempName,
+						Normalizer.Form.NFD);
 				norm = norm.replaceAll("[^\\p{ASCII}]", "");
-				teamList .add(norm.trim());
+				teamList.add(norm.trim());
 			}
 			System.out.println(teamList);
 			return teamList;
@@ -46,7 +55,5 @@ public class ClassicKaderFactory {
 			throw new RuntimeException("abbruch", e);
 		}
 	}
-
-	
 
 }
