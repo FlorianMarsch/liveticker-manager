80d042689fe57473efad6e9c4aec7641f6cfa468
Florian <dekontaminator@googlemail.com>
2016-04-23 18:38:03 +0200

    extends trainer with image url

--- src/main/java/de/fussballmanager/db/entity/trainer/Trainer.java
+++ src/main/java/de/fussballmanager/db/entity/trainer/Trainer.java
@@ -22,6 +22,17 @@ public class Trainer extends AbstractEntity {
 	@Column
 	private Boolean fake = Boolean.FALSE;
 
+	@Column
+	private String imageUrl;
+	
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
 	public Boolean getFake() {
 		return fake;
 	}
