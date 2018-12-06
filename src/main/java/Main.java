4ba220aeb818c9832952cbd5c318591c84fc654e
florianmarsch <florianmarsch@192.168.0.144>
2018-12-06 14:27:03 +0100

    test wirh options

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,6 +30,7 @@ import de.florianmarsch.fussballmanager.live.ticker.Event;
 import de.florianmarsch.fussballmanager.live.ticker.GoalResolver;
 import spark.ModelAndView;
 import spark.Request;
+import spark.Spark;
 import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
@@ -292,6 +293,16 @@ public class Main {
 			
 			return new ModelAndView(attributes, "screen.ftl");
 		} , new FreeMarkerEngine());
+		
+		Spark.options("*",  (request, response) -> {
+			response.status(200);
+			response.header("Content-Type", "application/json");
+			
+			response.header("Access-Control-Allow-Origin","*");
+			response.header("Access-Control-Allow-Methods","POST, GET, OPTIONS"); 
+			response.header("Access-Control-Allow-Headers","Content-Type"); 
+			return mapper.writeValueAsString(new HashMap<>());
+		});
 
 	}
 
